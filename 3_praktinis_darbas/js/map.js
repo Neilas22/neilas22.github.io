9bed791891050a6861f7e38997737429d1b2270b
Neilas22 <neilas.zarskas@chgf.stud.vu.lt>
2024-12-18 17:59:51 +0200

    Papildytas index.html ir map.js

--- 3_praktinis_darbas/js/map.js
+++ 3_praktinis_darbas/js/map.js
@@ -25,11 +25,24 @@ function switchBgLayers(layerName) {
 
     // MapLibre setStyle funkcija pakeičia žemėlapio stilių
     map.setStyle(layer);
+
+    document.getElementById("flexSwitchCheckDefault1").checked = false;
+    document.getElementById("flexSwitchCheckDefault2").checked = false;
+    document.getElementById("flexSwitchCheckDefault3").checked = false;
+
+    setTimeout(() => {
+        loadThematicLayers();
+      }, "1000");
 }
 
 map.on("load", () => {
     console.log("Žemėlapis užsikrovė");
 
+    loadThematicLayers();
+});
+
+function loadThematicLayers() {
+    // Pridedami žemėlapio šaltiniai kaip WMS el. paslaugos
     map.addSource("hidroelektrines-source",{
         type: "raster",
         tiles:[
@@ -37,14 +50,102 @@ map.on("load", () => {
         ],
         tilesize: 128,
     });
+
+    map.addSource("medziokles_plotu_vienetai-source",{
+        type: "raster",
+        tiles:[
+            "http://localhost/qgisserver/medziokles_plotu_vienetu_zemelapis?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=Medziokles_plotu_vienetai"
+        ],
+        tilesize: 128,
+    });
+
+    map.addSource("upes-source",{
+        type: "raster",
+        tiles:[
+            "http://localhost/qgisserver/upes_zemelapis?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=Upes"
+        ],
+        tilesize: 128,
+    });
+
+    // Pridedami žemėlapio sluoksniai iš žemėlapio šaltinių
     map.addLayer(
         {
             id: "hidroelektrines-wms",
             type: "raster",
             source: "hidroelektrines-source",
             layout: {
-                visibility: "visible",
+                visibility: "none",
             }
         }
     );
-});
+
+    map.addLayer(
+        {
+            id: "medziokles_plotu_vienetai-wms",
+            type: "raster",
+            source: "medziokles_plotu_vienetai-source",
+            layout: {
+                visibility: "none",
+            }
+        }
+    );
+
+    map.addLayer(
+        {
+            id: "upes-wms",
+            type: "raster",
+            source: "upes-source",
+            layout: {
+                visibility: "none",
+            }
+        }
+    );
+}
+
+function toggleLayers(layerId) {
+    if (map.getLayoutProperty(layerId, "visibility") == "none") {
+        // Įjungti sluoksnį
+        map.setLayoutProperty(layerId, "visibility", "visible")
+    } else {
+        // Išjungti sluoksnį
+        map.setLayoutProperty(layerId, "visibility", "none")
+    }
+}
+
+map.addControl(new maplibreGLMeasures.default({
+    lang: {
+        areaMeasurementButtonTitle: 'Matuoti plotą',
+        lengthMeasurementButtonTitle: 'Matuoti ilgį',
+        clearMeasurementsButtonTitle:  'Ištrinti matavimus',
+    },
+    units: 'metric',
+    unitsGroupingSeparator: ' ',
+    style: {
+        text: {
+            radialOffset:  0.9,
+            letterSpacing: 0.05,
+            color: '#D20C0C',
+            haloColor: '#fff',
+            haloWidth: 0,
+            font: 'Noto Sans Regular',
+        },
+        common: {
+            midPointRadius: 3,
+            midPointColor: '#D20C0C',
+            midPointHaloRadius: 5,
+            midPointHaloColor: '#FFF',
+        },
+        areaMeasurement: {
+            fillColor: '#D20C0C',
+            fillOutlineColor: '#D20C0C',
+            fillOpacity: 0.01,
+            lineWidth: 2,
+        },
+        lengthMeasurement: {
+            lineWidth: 2,
+            lineColor: "#D20C0C",
+        },
+    }
+}), 
+    "top-left",
+);
